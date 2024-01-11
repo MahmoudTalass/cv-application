3efd324289ef682a4630abc2fb2bbae3be41b074
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 18:14:41 -0500

    Create the inputs for the education info form

---
+++ src/components/formComponents/EducationInfoForm.jsx
@@ -0,0 +1,36 @@
+export default function EducationInfoForm() {
+   return (
+      <form>
+         <div className="input-container">
+            <label htmlFor="degree">Degree:</label>
+            <input type="text" name="degree" id="degree" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="school">School:</label>
+            <input type="text" name="school" id="school" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="city">City:</label>
+            <input type="text" name="city" id="city" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="state">State:</label>
+            <input type="text" name="state" id="state" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="state">State:</label>
+            <input type="text" name="state" id="state" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="graduation-date">Graduation Date:</label>
+            <input
+               type="text"
+               name="graduation-date"
+               id="graduation-date"
+               maxLength={7}
+               placeholder="MM/YYYY"
+            />
+         </div>
+      </form>
+   );
+}
