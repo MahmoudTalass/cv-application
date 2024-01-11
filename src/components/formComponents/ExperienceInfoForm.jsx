c343a8641a59d74fc5d2aec29dcd63766e186f70
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 18:27:07 -0500

    Create the form input fields for experience info

---
+++ src/components/formComponents/ExperienceInfoForm.jsx
@@ -0,0 +1,46 @@
+export default function ExperienceInfoForm() {
+   return (
+      <form>
+         <h2>Experience:</h2>
+         <div className="input-container">
+            <label htmlFor="company">Company:</label>
+            <input type="text" name="company" id="company" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="position-title">Position Title:</label>
+            <input type="text" name="position-title" id="position-title" />
+         </div>
+         <div className="start-end-date-container">
+            <div className="input-container">
+               <label htmlFor="start-date">Start Date</label>
+               <input
+                  type="text"
+                  name="start-date"
+                  id="start-date"
+                  maxLength={7}
+                  placeholder="MM/YYYY"
+               />
+            </div>
+            <div className="input-container">
+               <label htmlFor="end-date">End Date</label>
+               <input
+                  type="text"
+                  name="end-date"
+                  id="end-date"
+                  maxLength={7}
+                  placeholder="MM/YYYY"
+               />
+            </div>
+         </div>
+         <div className="input-container">
+            <label htmlFor="description">Description</label>
+            <textarea
+               name="description"
+               id="description"
+               cols="30"
+               rows="10"
+            ></textarea>
+         </div>
+      </form>
+   );
+}
