df6ce2bd271ce263233a89e386019a3497987100
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 18:06:34 -0500

    Create a form for personal info

---
+++ src/components/formComponents/PersonalInfoForm.jsx
@@ -0,0 +1,18 @@
+export default function PersonalInfoForm() {
+   return (
+      <form className="personalInfoForm form">
+         <div className="input-container">
+            <label htmlFor="">Name:</label>
+            <input type="text" name="name" id="name" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" />
+         </div>
+         <div className="input-container">
+            <label htmlFor="phone-number">Email:</label>
+            <input type="tel" name="phone-number" id="phone-number" />
+         </div>
+      </form>
+   );
+}
