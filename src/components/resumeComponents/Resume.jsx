79e1905745d71f83072248151127d08dca79c63a
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 19:44:13 -0500

    Create a component to have the whole resume

--- src/components/resumeComponents/Resume.jsx
+++ src/components/resumeComponents/Resume.jsx
@@ -1,1 +1,13 @@
-function Resume() {}
+import EducationInfo from "./EducationInfo";
+import ExperienceInfo from "./ExperienceInfo";
+import PersonalInfo from "./PersonalInfo";
+
+export default function Resume() {
+   return (
+      <section className="resume-container">
+         <PersonalInfo />
+         <EducationInfo />
+         <ExperienceInfo />
+      </section>
+   );
+}
