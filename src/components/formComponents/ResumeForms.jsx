72da17a21952cdcd16d5969a4f5a3f7232e45bd8
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 19:12:49 -0500

    Load the forms into one component

---
+++ src/components/formComponents/ResumeForms.jsx
@@ -0,0 +1,13 @@
+import EducationInfoForm from "./EducationInfoForm";
+import ExperienceInfoForm from "./ExperienceInfoForm";
+import PersonalInfoForm from "./PersonalInfoForm";
+
+export default function ResumeForms() {
+   return (
+      <>
+         <PersonalInfoForm />
+         <EducationInfoForm />
+         <ExperienceInfoForm />
+      </>
+   );
+}
