6ae03267450f56f3181d227f1a3837bc65ea9e90
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 22:49:00 -0500

    Call the handlePersonalInfo function when the form is submitted
    Make all the field required and use a local variable to keep hold of the
    current values in the form input fields and retrieve them when the
    handle function is called

--- src/components/formComponents/PersonalInfoForm.jsx
+++ src/components/formComponents/PersonalInfoForm.jsx
@@ -1,18 +1,53 @@
-export default function PersonalInfoForm() {
+/* eslint-disable react/prop-types */
+export default function PersonalInfoForm({ handlePersonalInfo, personalInfo }) {
+   let currentInfo = {};
    return (
-      <form className="personalInfoForm form">
+      <form
+         className="personalInfoForm form"
+         onSubmit={(e) => {
+            e.preventDefault();
+            handlePersonalInfo(currentInfo);
+         }}
+      >
          <h2>Personal Details</h2>
          <div className="input-container">
             <label htmlFor="">Name:</label>
-            <input type="text" name="name" id="name" />
+            <input
+               type="text"
+               name="name"
+               id="name"
+               required
+               value={personalInfo.name}
+               onChange={(e) => {
+                  currentInfo.name = e.target.value;
+               }}
+            />
          </div>
          <div className="input-container">
             <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" />
+            <input
+               type="email"
+               name="email"
+               id="email"
+               required
+               value={personalInfo.email}
+               onChange={(e) => {
+                  currentInfo.email = e.target.value;
+               }}
+            />
          </div>
          <div className="input-container">
             <label htmlFor="phone-number">Phone Number:</label>
-            <input type="tel" name="phone-number" id="phone-number" />
+            <input
+               type="tel"
+               name="phone-number"
+               id="phone-number"
+               required
+               value={personalInfo.phoneNumber}
+               onChange={(e) => {
+                  currentInfo.phoneNumber = e.target.value;
+               }}
+            />
          </div>
          <div className="input-container">
             <label htmlFor="location">Location:</label>
@@ -21,9 +56,13 @@ export default function PersonalInfoForm() {
                name="location"
                id="location"
                placeholder="city, state"
+               required
+               value={personalInfo.location}
+               onChange={(e) => {
+                  currentInfo.location = e.target.value;
+               }}
             />
          </div>
-
          <button type="submit">Submit</button>
       </form>
    );
