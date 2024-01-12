09be9e4b6e3ddbd7fcb2344c45d4fbdaba8b0bcf
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 22:52:12 -0500

    Receive the props from the parent component and pass them to the forms

--- src/components/formComponents/ResumeForms.jsx
+++ src/components/formComponents/ResumeForms.jsx
@@ -1,13 +1,30 @@
+/* eslint-disable react/prop-types */
 import EducationInfoForm from "./EducationInfoForm";
 import ExperienceInfoForm from "./ExperienceInfoForm";
 import PersonalInfoForm from "./PersonalInfoForm";
 
-export default function ResumeForms() {
+export default function ResumeForms({
+   handlePersonalInfo,
+   handleEducationInfo,
+   handleExperienceInfo,
+   personalInfo,
+   educationInfo,
+   experienceInfo,
+}) {
    return (
       <section className="forms-container">
-         <PersonalInfoForm />
-         <EducationInfoForm />
-         <ExperienceInfoForm />
+         <PersonalInfoForm
+            handlePersonalInfo={handlePersonalInfo}
+            personalInfo={personalInfo}
+         />
+         <EducationInfoForm
+            handleEducationInfo={handleEducationInfo}
+            educationInfo={educationInfo}
+         />
+         <ExperienceInfoForm
+            handleExperienceInfo={handleExperienceInfo}
+            experienceInfo={experienceInfo}
+         />
       </section>
    );
 }
