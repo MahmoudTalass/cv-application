e0f6d69c93d4fc23ae2b06e267b5ead58dbe6396
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 20:28:40 -0500

    Create state and functions to handle state change and pass them as props
    The states handle personal info, education info, and experience info

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,52 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ResumeForms from "./components/formComponents/ResumeForms";
 import Resume from "./components/resumeComponents/Resume";
+import { useState } from "react";
 
 export default function App() {
+   const [personalInfo, setPersonalInfo] = useState({});
+   const [educationInfo, setEducationInfo] = useState([]);
+   const [experienceInfo, setExperienceInfo] = useState([]);
+
+   function handlePersonalInfo(name, email, phoneNumber) {
+      setPersonalInfo({ name, email, phoneNumber });
+   }
+
+   function handleEducationInfo(degree, school, city, state, graduationDate) {
+      setEducationInfo([
+         ...educationInfo,
+         { degree, school, city, state, graduationDate },
+      ]);
+   }
+
+   function handleExperienceInfo(
+      company,
+      position,
+      startDate,
+      endDate,
+      description
+   ) {
+      setExperienceInfo([
+         ...experienceInfo,
+         { company, position, startDate, endDate, description },
+      ]);
+   }
+
    return (
       <div className="page-container">
          <Header />
          <main className="main-content">
-            <ResumeForms />
-            {/* <Resume /> */}
+            <ResumeForms
+               handlePersonalInfo={handlePersonalInfo}
+               handleEducationInfo={handleEducationInfo}
+               handleExperienceInfo={handleExperienceInfo}
+               personalInfo={personalInfo}
+               educationInfo={educationInfo}
+               experienceInfo={experienceInfo}
+            />
+            {/* <Resume personalInfo={personalInfo}
+               educationInfo={educationInfo}
+               experienceInfo={experienceInfo} /> */}
          </main>
          <Footer />
       </div>
