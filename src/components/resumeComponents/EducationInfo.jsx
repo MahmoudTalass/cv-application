73f1e8c27a7d5427182972497ffb1cd0f0ea2bc2
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 19:32:01 -0500

    Create the education section for resume

---
+++ src/components/resumeComponents/EducationInfo.jsx
@@ -0,0 +1,22 @@
+export default function EducationInfo({ educationInfo }) {
+   return (
+      <div className="education-info-section resume-section">
+         <p className="section-heading">EDUCATION</p>
+         <div className="line-break"></div>
+         {educationInfo.map((info) => {
+            <div className="education-info-container" key={info.id}>
+               <div className="line-1-education">
+                  <p className="school-name">{info.school}</p>
+                  <p className="location">
+                     {info.city}, {info.state}
+                  </p>
+               </div>
+               <div className="line-2-eduation">
+                  <p className="degree">{info.degree}</p>
+                  <p className="graduation-date">{info.graduationDate}</p>
+               </div>
+            </div>;
+         })}
+      </div>
+   );
+}
