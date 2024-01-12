61cadb5132cf59b81caffe3c5932bdc6f87eca1b
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 19:49:08 -0500

    Add footer to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import ResumeForms from "./components/formComponents/ResumeForms";
+import Resume from "./components/resumeComponents/Resume";
 
 export default function App() {
    return (
       <>
          <Header />
          <ResumeForms />
+         {/* <Resume /> */}
+         <Footer />
       </>
    );
 }
