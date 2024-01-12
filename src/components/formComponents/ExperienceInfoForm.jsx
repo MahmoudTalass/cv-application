7395af23270fe81c07d56ed26a3186bf944c0898
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-12 00:23:45 -0500

    Add state to handle input and change experienceInfo state on submit

--- src/components/formComponents/ExperienceInfoForm.jsx
+++ src/components/formComponents/ExperienceInfoForm.jsx
@@ -1,14 +1,39 @@
-export default function ExperienceInfoForm() {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export default function ExperienceInfoForm({ handleExperienceInfo }) {
+   const [currentInfo, setCurrentInfo] = useState({});
+
+   function handleChange(e, key) {
+      setCurrentInfo({ ...currentInfo, [key]: e.target.value });
+   }
+
    return (
-      <form className="personalInfoForm form">
+      <form
+         className="personalInfoForm form"
+         onSubmit={(e) => {
+            e.preventDefault();
+            handleExperienceInfo(currentInfo);
+         }}
+      >
          <h2>Experience:</h2>
          <div className="input-container">
             <label htmlFor="company">Company:</label>
-            <input type="text" name="company" id="company" />
+            <input
+               type="text"
+               name="company"
+               id="company"
+               onChange={(e) => handleChange(e, "company")}
+            />
          </div>
          <div className="input-container">
             <label htmlFor="position-title">Position Title:</label>
-            <input type="text" name="position-title" id="position-title" />
+            <input
+               type="text"
+               name="position-title"
+               id="position-title"
+               onChange={(e) => handleChange(e, "position")}
+            />
          </div>
          <div className="start-end-date-container">
             <div className="input-container">
@@ -19,6 +44,7 @@ export default function ExperienceInfoForm() {
                   id="start-date"
                   maxLength={7}
                   placeholder="MM/YYYY"
+                  onChange={(e) => handleChange(e, "startDate")}
                />
             </div>
             <div className="input-container">
@@ -29,6 +55,7 @@ export default function ExperienceInfoForm() {
                   id="end-date"
                   maxLength={7}
                   placeholder="MM/YYYY"
+                  onChange={(e) => handleChange(e, "endDate")}
                />
             </div>
          </div>
@@ -39,6 +66,7 @@ export default function ExperienceInfoForm() {
                id="description"
                cols="30"
                rows="10"
+               onChange={(e) => handleChange(e, "description")}
             ></textarea>
          </div>
 
