b62a40635ab6b0ad1ffd7128ec81e55b292d1e37
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-12 00:30:01 -0500

    When mapping over the info, return the jsx

--- src/components/resumeComponents/ExperienceInfo.jsx
+++ src/components/resumeComponents/ExperienceInfo.jsx
@@ -5,25 +5,27 @@ export default function ExperienceInfo({ experienceInfo }) {
          <p className="section-heading">EXPERIENCE</p>
          <div className="line-break"></div>
          {experienceInfo.map((info) => {
-            <div className="experience-info-container" key={info.id}>
-               <div className="line-1-container">
-                  <p className="company">{info.company}</p>
-                  <p className="location">{info.location}</p>
+            return (
+               <div className="experience-info-container" key={info.id}>
+                  <div className="line-1-container">
+                     <p className="company">{info.company}</p>
+                     <p className="location">{info.location}</p>
+                  </div>
+                  <div className="line-2-container">
+                     <p className="position">{info.position}</p>
+                     <p className="start-end-dates">
+                        {info.startDate} - {info.endDate}
+                     </p>
+                  </div>
+                  <div className="line-2-container">
+                     <p className="position">{info.position}</p>
+                     <p className="start-end-dates">
+                        {info.startDate} - {info.endDate}
+                     </p>
+                  </div>
+                  <p className="job-description">{info.description}</p>
                </div>
-               <div className="line-2-container">
-                  <p className="position">{info.position}</p>
-                  <p className="start-end-dates">
-                     {info.startDate} - {info.endDate}
-                  </p>
-               </div>
-               <div className="line-2-container">
-                  <p className="position">{info.position}</p>
-                  <p className="start-end-dates">
-                     {info.startDate} - {info.endDate}
-                  </p>
-               </div>
-               <p className="job-description">{info.description}</p>
-            </div>;
+            );
          })}
       </div>
    );
