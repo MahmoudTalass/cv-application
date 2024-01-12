243754e05f70fa0356c76fe1ddba6970ee300fd5
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-12 16:50:30 -0500

    Add toggles for forms and the list of each info section
    The personal details original form only displays the first time personal
    are being entered and then later, an edit button will be added.

--- src/components/formComponents/ResumeForms.jsx
+++ src/components/formComponents/ResumeForms.jsx
@@ -8,22 +8,119 @@ export default function ResumeForms({
    handlePersonalInfo,
    handleEducationInfo,
    handleExperienceInfo,
+   personalInfo,
+   educationInfo,
+   experienceInfo,
 }) {
+   const [displayForms, setDisplayForms] = useState({
+      showPersonalForm: false,
+      showEducationForm: false,
+      showExperienceForm: false,
+   });
+
+   const [displayInfo, setDisplayInfo] = useState({
+      showPersonalInfo: false,
+      showEducationList: false,
+      showExperienceList: false,
+   });
+
+   const [isPersonalInfoFilled, setIsPersonalInfoFilled] = useState(false);
+
+   function handleFirstPersonalInfoForm() {
+      setIsPersonalInfoFilled(true);
+   }
+
+   function handleDisplayForms(formName) {
+      setDisplayForms({
+         ...displayForms,
+         [formName]: !displayForms[formName],
+      });
+   }
+
+   function handleDisplayInfo(infoSection) {
+      setDisplayInfo({
+         ...displayInfo,
+         [infoSection]: !displayInfo[infoSection],
+      });
+   }
+
    return (
       <section className="forms-container">
          <div>
-            <button>Personal Details</button>
-            <PersonalInfoForm handlePersonalInfo={handlePersonalInfo} />
+            <button
+               className="display-current-info-btn"
+               onClick={() => handleDisplayInfo("showPersonalInfo")}
+            >
+               Personal Details:
+            </button>
+            {displayInfo.showPersonalInfo && (
+               <>
+                  {!isPersonalInfoFilled && (
+                     <PersonalInfoForm
+                        handlePersonalInfo={handlePersonalInfo}
+                        handleDisplayForms={handleDisplayForms}
+                        handleFirstPersonalInfoForm={
+                           handleFirstPersonalInfoForm
+                        }
+                     />
+                  )}
+
+                  <div className="personalInfoDisplay">
+                     {personalInfo.name} {personalInfo.email}{" "}
+                     {personalInfo.phoneNumber} {personalInfo.location}
+                     info
+                  </div>
+               </>
+            )}
          </div>
          <div>
-            <button>Education:</button>
-            <EducationInfoForm handleEducationInfo={handleEducationInfo} />
-            <ul className="education-list"></ul>
+            <button
+               className="display-current-info-btn"
+               onClick={() => handleDisplayInfo("showEducationList")}
+            >
+               Education:
+            </button>
+
+            {displayInfo.showEducationList && (
+               <>
+                  <ul className="education-list"></ul>
+                  {!displayForms.showEducationForm && (
+                     <button
+                        onClick={() => handleDisplayForms("showEducationForm")}
+                     >
+                        Add
+                     </button>
+                  )}
+                  {displayForms.showEducationForm && (
+                     <EducationInfoForm
+                        handleEducationInfo={handleEducationInfo}
+                     />
+                  )}
+               </>
+            )}
          </div>
          <div>
-            <h2>Experience:</h2>
-            <ExperienceInfoForm handleExperienceInfo={handleExperienceInfo} />
-            <ul className="experiences-list"></ul>
+            <button
+               className="display-current-info-btn"
+               onClick={() => handleDisplayInfo("showExperienceList")}
+            >
+               Experience:
+            </button>
+            {displayInfo.showExperienceList && (
+               <>
+                  <ul className="experiences-list"> </ul>
+                  <button
+                     onClick={() => handleDisplayForms("showExperienceForm")}
+                  >
+                     Add
+                  </button>
+               </>
+            )}
+            {displayForms.showExperienceForm && (
+               <ExperienceInfoForm
+                  handleExperienceInfo={handleExperienceInfo}
+               />
+            )}
          </div>
       </section>
    );
