8ad4658d801448ac7d2bbc5cc4ed5e798b323eab
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-11 19:13:16 -0500

    Load forms into page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import ResumeForms from "./components/formComponents/ResumeForms";
 
 export default function App() {
    return (
       <>
          <Header />
+         <ResumeForms />
       </>
    );
 }
