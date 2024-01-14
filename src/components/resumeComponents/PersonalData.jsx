ac4d95beb50757f77c5c521e1c303aa88bea6817
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-14 13:56:39 -0500

    use lower case d in data for consistency

--- src/components/resumeComponents/PersonalData.jsx
+++ src/components/resumeComponents/PersonalData.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 export default function PersonalData({ personalData }) {
    return (
-      <div className="personal-Data-section resume-section">
+      <div className="personal-data-section resume-section">
          <p>{personalData.name}</p>
-         <div className="contact-Data-container">
+         <div className="contact-data-container">
             <p>{personalData.location}</p>
             <p>{personalData.email}</p>
             <p>{personalData.phoneNumber}</p>
