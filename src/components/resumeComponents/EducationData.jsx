5807700e5b84c38a32229c500f3f4a0fab60f895
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-14 14:26:54 -0500

    Put the list of education and experiences in a container
    Give elements classes for styling

--- src/components/resumeComponents/EducationData.jsx
+++ src/components/resumeComponents/EducationData.jsx
@@ -4,22 +4,27 @@ export default function EducationData({ educationData }) {
       <div className="education-data-section resume-section">
          <p className="section-heading">EDUCATION</p>
          <div className="line-break"></div>
-         {educationData.map((data) => {
-            return (
-               <div className="education-data-container" key={data.id}>
-                  <div className="line-1-education">
-                     <p className="school-name">{data.school}</p>
-                     <p className="location">
-                        {data.city}, {data.state}
-                     </p>
+         <div className="education-data-list resume-data-list">
+            {educationData.map((data) => {
+               return (
+                  <div
+                     className="education-data-container resume-data-container"
+                     key={data.id}
+                  >
+                     <div className="line-1-container">
+                        <p className="school-name">{data.school}</p>
+                        <p className="location">
+                           {data.city}, {data.state}
+                        </p>
+                     </div>
+                     <div className="line-2-container">
+                        <p className="degree">{data.degree}</p>
+                        <p className="graduation-date">{data.graduationDate}</p>
+                     </div>
                   </div>
-                  <div className="line-2-eduation">
-                     <p className="degree">{data.degree}</p>
-                     <p className="graduation-date">{data.graduationDate}</p>
-                  </div>
-               </div>
-            );
-         })}
+               );
+            })}
+         </div>
       </div>
    );
 }
--- src/components/resumeComponents/ExperienceData.jsx
+++ src/components/resumeComponents/ExperienceData.jsx
@@ -4,23 +4,28 @@ export default function ExperienceData({ experienceData }) {
       <div className="experience-data-section resume-section">
          <p className="section-heading">EXPERIENCE</p>
          <div className="line-break"></div>
-         {experienceData.map((data) => {
-            return (
-               <div className="experience-data-container" key={data.id}>
-                  <div className="line-1-container">
-                     <p className="company">{data.company}</p>
-                     <p className="location">{data.location}</p>
+         <div className="experience-data-list resume-data-list">
+            {experienceData.map((data) => {
+               return (
+                  <div
+                     className="experience-data-container resume-data-container"
+                     key={data.id}
+                  >
+                     <div className="line-1-container">
+                        <p className="company">{data.company}</p>
+                        <p className="location">{data.location}</p>
+                     </div>
+                     <div className="line-2-container">
+                        <p className="position">{data.position}</p>
+                        <p className="start-end-dates">
+                           {data.startDate} - {data.endDate}
+                        </p>
+                     </div>
+                     <p className="job-description">{data.description}</p>
                   </div>
-                  <div className="line-2-container">
-                     <p className="position">{data.position}</p>
-                     <p className="start-end-dates">
-                        {data.startDate} - {data.endDate}
-                     </p>
-                  </div>
-                  <p className="job-description">{data.description}</p>
-               </div>
-            );
-         })}
+               );
+            })}
+         </div>
       </div>
    );
 }
