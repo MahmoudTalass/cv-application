a6e2798414844216ef25cf34df1fd072f496ae55
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-14 14:26:34 -0500

    Add classes for styling

--- src/components/resumeComponents/PersonalData.jsx
+++ src/components/resumeComponents/PersonalData.jsx
@@ -2,8 +2,8 @@
 export default function PersonalData({ personalData }) {
    return (
       <div className="personal-data-section resume-section">
-         <p>{personalData.name}</p>
-         <div className="contact-data-container">
+         <p className="name">{personalData.name}</p>
+         <div className="contact-info-container">
             <p>{personalData.location}</p>
             <p>{personalData.email}</p>
             <p>{personalData.phoneNumber}</p>
