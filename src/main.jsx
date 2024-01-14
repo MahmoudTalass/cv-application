aab2c0c61749ec621c91e229b04af93f24bcb5d4
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-14 16:22:07 -0500

    Change css folder name to styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./css/page-layout.css";
-import "./css/forms.css";
-import "./css/resume.css";
+import "./styles/page-layout.css";
+import "./styles/forms.css";
+import "./styles/resume.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
