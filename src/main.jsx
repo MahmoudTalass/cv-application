4b1e0d4a268dd75f62eb4584478fc33ff4bd405d
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-14 19:39:02 -0500

    import cards css file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/page-layout.css";
 import "./styles/forms.css";
+import "./styles/cards.css";
 import "./styles/resume.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
